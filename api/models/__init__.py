5048affe890a7df613c80971a896ce1e2883154d
SNeXT <snext2004@gmail.com>
2025-02-12 13:41:37 +0700

    split models

---
+++ api/models/__init__.py
@@ -0,0 +1,4 @@
+from .course import Course
+from .user import CustomUser
+from .curriculum import Curriculum, CurriculumMapping
+from .note import Note
---
+++ api/models/course.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# ✅ Course Model (No Year/Term Here!)
+class Course(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)  
+    name = models.CharField(max_length=255)  
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
